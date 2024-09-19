86531dcf23cef715a8158414c633385ec015d386
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-09-19 14:40:20 +0200

    add ex18

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: estettle <stettler.etan@protonmail.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 14:34:12 by estettle          #+#    #+#             */
+/*   Updated: 2024/09/19 14:38:36 by estettle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		ft_putchar(str[i++]);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	while (++i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+	}
+}
