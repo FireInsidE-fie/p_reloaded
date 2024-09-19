1f6b24cf0dbfc38999a1e5d281d69d5cc7657016
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-09-19 13:46:00 +0200

    add ex15

---
+++ ex15/ft_putstr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: estettle <stettler.etan@protonmail.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 13:39:30 by estettle          #+#    #+#             */
+/*   Updated: 2024/09/19 13:45:50 by estettle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		ft_putchar(str[i++]);
+}
+/*
+int	main(void)
+{
+	ft_putstr("Can. You. Hear. Me?");
+}
+*/
