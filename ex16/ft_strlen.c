1856f3e9a8a7f90af032ad4404c4eadcd58b53a1
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-09-19 13:50:27 +0200

    add ex16

---
+++ ex16/ft_strlen.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: estettle <stettler.etan@protonmail.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 13:49:05 by estettle          #+#    #+#             */
+/*   Updated: 2024/09/19 13:50:21 by estettle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%d\n", ft_strlen("Can. You. Hear. Me?"));
+}
+*/
