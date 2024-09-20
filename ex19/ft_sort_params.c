3402670a80ebf54c5c3428b3d46773367a2fd193
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-09-20 10:04:35 +0200

    getting closer on ex19

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: estettle <stettler.etan@protonmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 14:42:36 by estettle          #+#    #+#             */
-/*   Updated: 2024/09/20 09:49:45 by estettle         ###   ########.fr       */
+/*   Updated: 2024/09/20 10:04:25 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,29 +21,24 @@ void	ft_putstr(char *str)
 		ft_putchar(str[i++]);
 }
 
-void	ft_swap(char *s1, char *s2)
-{
-	char	*tmp;
-
-	tmp = s1;
-	s1 = s2;
-	s2 = tmp;
-}
-
 char	**ft_sort(int size, char **strings)
 {
-	int	i;
-	int	j;
+	int		i;
+	int		j;
+	char	*tmp;
 
 	i = 1;
 	while (i < size - 1)
 	{
 		j = 0;
+		tmp = 0;
 		while (strings[i][j])
 		{
 			if (strings[i][j] > strings[i + 1][j])
 			{
-				ft_swap(strings[i], strings[i + 1]);
+				tmp = strings[i + 1];
+				strings[i] = strings[i + 1];
+				strings[i + 1] = tmp;
 				break ;
 			}
 			j++;
