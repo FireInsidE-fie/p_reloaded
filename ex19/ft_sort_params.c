41caf72354e88791fac20ee2af1223f0a51cec53
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-09-20 10:39:43 +0200

    i finally understand how pointers to strings work

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: estettle <stettler.etan@protonmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 14:42:36 by estettle          #+#    #+#             */
-/*   Updated: 2024/09/20 10:04:25 by estettle         ###   ########.fr       */
+/*   Updated: 2024/09/20 10:38:00 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,24 +21,29 @@ void	ft_putstr(char *str)
 		ft_putchar(str[i++]);
 }
 
+void	ft_swap(char **s1, char **s2)
+{
+	char	*tmp;
+
+	tmp = *s2;
+	*s2 = *s1;
+	*s1 = tmp;
+}
+
 char	**ft_sort(int size, char **strings)
 {
 	int		i;
 	int		j;
-	char	*tmp;
 
 	i = 1;
 	while (i < size - 1)
 	{
 		j = 0;
-		tmp = 0;
 		while (strings[i][j])
 		{
 			if (strings[i][j] > strings[i + 1][j])
 			{
-				tmp = strings[i + 1];
-				strings[i] = strings[i + 1];
-				strings[i + 1] = tmp;
+				ft_swap(&strings[i], &strings[i + 1]);
 				break ;
 			}
 			j++;
